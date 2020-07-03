7d981f7335cc8a51128234ea126da54c6cfd94cb
Thomas Ngo <tngo0508@gmai.com>
2020-07-02 19:23:13 -0700

    responding with data using http module

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require("http");
 
+const todos = [
+  { id: 1, text: "Todo One" },
+  { id: 1, text: "Todo One" },
+  { id: 1, text: "Todo Three" },
+];
+
 const server = http.createServer((req, res) => {
-  res.end();
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("X-Powered-By", "Node.js");
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos,
+    })
+  );
 });
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
